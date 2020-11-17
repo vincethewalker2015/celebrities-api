bef65f5bb1cd674266b623995bffcbe6d1f296fd
Vincent <vincethewalker@yahoo.co.uk>
2020-11-17 15:14:31 +0000

    Created Model, Controller and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Celebrity.create(name: "Oprah Winfrey", wiki_page: "https://en.wikipedia.org/wiki/Oprah_Winfrey")
+Celebrity.create(name: "Adele", wiki_page: "https://en.wikipedia.org/wiki/Adele")
+Celebrity.create(name: "Amelia Earhart", wiki_page: "https://en.wikipedia.org/wiki/Amelia_Earhart")
+Celebrity.create(name: "J. K. Rowling", wiki_page: "https://en.wikipedia.org/wiki/J._K._Rowling")
+Celebrity.create(name: "Rihanna", wiki_page: "https://en.wikipedia.org/wiki/Rihanna")
+Celebrity.create(name: "Lewis Hamilton", wiki_page: "https://en.wikipedia.org/wiki/Lewis_Hamilton")
+Celebrity.create(name: "Rory McIllroy", wiki_page: "https://en.wikipedia.org/wiki/Rory_McIlroy")
+Celebrity.create(name: "Richard Branson", wiki_page: "https://en.wikipedia.org/wiki/Richard_Branson")
+Celebrity.create(name: "Elon Musk", wiki_page: "https://en.wikipedia.org/wiki/Elon_Musk")
+Celebrity.create(name: "Nick Ferrari", wiki_page: "https://en.wikipedia.org/wiki/Nick_Ferrari")
